2ca66f563935793308d49afca79c49c324e20cde
Cygra <sjtuwbh@gmail.com>
2025-02-14 10:46:24 +0800

    fix: use ref to schedule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,59 @@
 "use client";
 
 import { useRef } from "react";
-import { usePlay } from "./usePlay";
 
 export default function Home() {
   const hourRef = useRef<HTMLAudioElement>(null);
   const quarterRef = useRef<HTMLAudioElement>(null);
   const minuteRef = useRef<HTMLAudioElement>(null);
 
-  const { play: playHour } = usePlay(hourRef.current);
-  const { play: playQuarter } = usePlay(quarterRef.current);
-  const { play: playMinute } = usePlay(minuteRef.current);
+  const listRef = useRef<string[]>(["hour", "hour"]);
 
   const playAudio = () => {
     const date = new Date();
+
     let hour = date.getHours();
     if (hour === 0) hour = 12;
+
     const quarter = Math.floor(date.getMinutes() / 15);
+
     const minute = date.getMinutes() % 15;
 
-    playHour(hour, () => {
-      playQuarter(quarter, () => {
-        playMinute(minute);
-      });
-    });
+    const list = [];
+
+    for (let i = 0; i < hour - 1; i++) {
+      list.push("hour");
+    }
+    for (let i = 0; i < quarter; i++) {
+      list.push("quarter");
+    }
+    for (let i = 0; i < minute; i++) {
+      list.push("minute");
+    }
+
+    listRef.current = list;
+    hourRef.current?.play();
+  };
+
+  const onAudioEnd = () => {
+    const [first, ...other] = listRef.current;
+    if (first) {
+      switch (first) {
+        case "hour":
+          hourRef.current?.play();
+          break;
+        case "quarter":
+          quarterRef.current?.play();
+          break;
+        case "minute":
+          minuteRef.current?.play();
+          break;
+        default:
+          break;
+      }
+
+      listRef.current = other;
+    }
   };
 
   return (
@@ -32,16 +62,19 @@ export default function Home() {
         ref={hourRef}
         src={"/minute-repeater-online/hour.aac"}
         preload="metadata"
+        onEnded={onAudioEnd}
       />
       <audio
         ref={quarterRef}
         src={"/minute-repeater-online/quarter.aac"}
         preload="metadata"
+        onEnded={onAudioEnd}
       />
       <audio
         ref={minuteRef}
         src={"/minute-repeater-online/minute.aac"}
         preload="metadata"
+        onEnded={onAudioEnd}
       />
       <div
         onClick={playAudio}
